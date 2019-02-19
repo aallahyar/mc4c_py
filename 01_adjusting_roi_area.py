51fbf63d4e2a19c2b65cac8b56254b228a2d32b9
aallahyar <aminbys@gmail.com>
2019-02-19 07:26:34 +0100

    minor edits to ROI adjuster

--- 01_adjusting_roi_area.py
+++ 01_adjusting_roi_area.py
@@ -89,12 +89,15 @@ for bi in range(n_bin):
     bin_frq[1, bi] = len(np.unique(frg_trs[is_in, 0]))
 
 # compare profiles
-plt.figure(figsize=[7, 7])
+plt.figure(figsize=[8, 7])
 plt.plot(bin_frq[0] / float(n_read[0]), bin_frq[1] / float(n_read[1]), 'o')
 plt.xlim([0, 0.1])
 plt.ylim([0, 0.1])
-plt.title('ROI coverage, far-cis + trans vs. trans only duplicate filter\n' +
+plt.title(configs['run_id'] + '\n' +
+          'ROI coverage, far-cis + trans vs. trans only duplicate filter\n' +
           'spr-corr: {:0.5f}'.format(spearmanr(bin_frq.T).correlation))
+out_fname = './plots/AdjustingROI_{:s}_'.format(configs['run_id'])
+plt.savefig(out_fname + 'binCvg_Cmp.pdf', bbox_inches='tight')
 
 # check coverage across the chromosome
 from utilities import get_chr_info
@@ -114,36 +117,30 @@ for bi in range(n_blk):
     blk_cvg[1, bi] = len(np.unique(frg_trs[is_in, 0]))
 
 # check similarity
-plt.figure(figsize=[7, 7])
+plt.figure(figsize=[8, 7])
 plt.plot(blk_cvg[0] / float(n_read[0]), blk_cvg[1] / float(n_read[1]), 'o')
 plt.xlim([0, 0.1])
 plt.ylim([0, 0.1])
 plt.title('Chromosome coverage, far-cis + trans vs. trans only duplicate filter\n' +
           'spr-corr: {:0.5f}'.format(spearmanr(blk_cvg.T).correlation))
+plt.savefig(out_fname + 'ChrCvg_Cmp.pdf', bbox_inches='tight')
 
 # plot chr coverage
+plt_h = [None] * 3
 plt.figure(figsize=[15, 5])
-plt.cla()
 # plt.plot(bin_bnd[:, 0], chr_cvg[0] / float(n_read[0]), '+', color='green')
 p0_pd = pd.Series(blk_cvg[0] / float(n_read[0]))
-plt.plot(blk_bnd[:, 0], p0_pd.rolling(5).mean().values, ':', color='green')
+plt_h[0] = plt.plot(blk_bnd[:, 0], p0_pd.rolling(5).mean().values, ':', color='green')[0]
 # plt.plot(bin_bnd[:, 0], chr_cvg[1] / float(n_read[1]), 'o', color='orange', markerfacecolor='None')
 p1_pd = pd.Series(blk_cvg[1] / float(n_read[1]))
-plt.plot(blk_bnd[:, 0], p1_pd.rolling(5).mean().values, ':', color='orange')
+plt_h[1] = plt.plot(blk_bnd[:, 0], p1_pd.rolling(5).mean().values, ':', color='orange')[0]
 plt.plot([roi_crd[1], roi_crd[1]], [0, 1], color='k')
 plt.plot([roi_crd[2], roi_crd[2]], [0, 1], color='k')
 plt.plot([lcl_crd[1], lcl_crd[1]], [0, 1], color='gray')
 plt.plot([lcl_crd[2], lcl_crd[2]], [0, 1], color='gray')
-plt.plot([edge_lst[0], edge_lst[-1]], [0.05, 0.05], '-.', color='lightgray')
-# plt.xlim(lcl_crd[1:])
+plt.plot([blk_lst[0], blk_lst[-1]], [0.05, 0.05], '-.', color='lightgray')
 plt.ylim([0, 0.1])
-plt.legend([
-    'Far-cis method (n={:0.0f})'.format(n_read[0]),
-    'Trans only (n={:0.0f})'.format(n_read[1])
-])
-plt.title('{:s}\n'.format(configs['run_id']) +
-          'bin_w={:0.0f}, block_w={:0.0f}k\n'.format(bin_w, blk_w / 1e3))
-
+plt.savefig(out_fname + 'ProfileChrom.pdf', bbox_inches='tight')
 
 # use adjusted area
 prf_rol = p1_pd.rolling(5).mean().values
@@ -158,13 +155,26 @@ is_adj = np.isin(read_all[:, 0], adj_set[:, 0])
 frg_adj = read_all[is_adj, :].copy()
 n_read.append(len(np.unique(frg_adj[:, 0])))
 print '\t#reads: {:,d} --> {:,d}'.format(len(np.unique(read_all[:, 0])), n_read[2])
+plt.xlim([adj_crd[1]-adj_w*3, adj_crd[2]+adj_w*3])
+plt.ylim([0, 0.1])
+plt.savefig(out_fname + 'ProfileLocal.pdf', bbox_inches='tight')
 
 # plots for adjust
-plt.plot([adj_crd[1], adj_crd[1]], [0, 1], color='red')
-plt.plot([adj_crd[2], adj_crd[2]], [0, 1], color='red')
+plt_h[2] = plt.plot([adj_crd[1], adj_crd[1]], [0, 1], color='red')[0]
+plt_h[2] = plt.plot([adj_crd[2], adj_crd[2]], [0, 1], color='red')[0]
 plt.plot([adj_crd[1]-adj_w, adj_crd[1]-adj_w], [0, 1], color='#fe9090')
 plt.plot([adj_crd[2]+adj_w, adj_crd[2]+adj_w], [0, 1], color='#fe9090')
+plt.xlim([adj_crd[1]-adj_w*3, adj_crd[2]+adj_w*3])
+plt.ylim([0, 0.1])
+plt.legend(plt_h, [
+    'Far-cis (n={:0.0f})'.format(n_read[0]),
+    'Trans only (n={:0.0f})'.format(n_read[1]),
+    'Adjusted (n={:0.0f})'.format(n_read[2])
+])
+plt.title('{:s}\n'.format(configs['run_id']) +
+          'bin_w={:0.0f}, block_w={:0.0f}k\n'.format(bin_w, blk_w / 1e3))
+plt.savefig(out_fname + 'ProfileLocal_withAdj.pdf', bbox_inches='tight')
 
 assert np.array_equal(read_all, mc4c_pd.loc[is_mapped & is_valid, header_lst[:-2]].values)
 print 'Done'
-plt.show()
+# plt.show()
